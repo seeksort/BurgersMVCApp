e4b339683a308a556175e351923d3ed2f175780a
Kristin Faner <kfaner@gmail.com>
2016-11-04 20:02:37 -0500

    fixes...

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'test1_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '',
+        database: 'test1_db'
+    });
+}
 
 // Set up and check connection
 connection.connect(function(err){
